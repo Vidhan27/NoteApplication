c31e2e22bd8012110034c7ba86171c27b1313c7a
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2023-01-04 22:15:38 +0530

    Initial commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This the React Bootcamp</h1>
+      <p>
+        All the students who complete the Bootcamp with complete attendance and
+        project submissions will be rewarded certificates from us.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
